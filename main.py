6adb8ec875d5cbb9d5f3b8267a119a152cca5785
RaimeiRaikiri <mikescutts10@gmail.com>
2025-03-23 16:15:06 +0000

    code to test and learn containers

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 
-
-response = requests.get("http://127.0.0.1:5000/contacts")
+payload = {
+"appid": API_KEY,
+"q": "Rainham",
+"units": "metric"}
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", payload)
 
 print(response.text)
 
